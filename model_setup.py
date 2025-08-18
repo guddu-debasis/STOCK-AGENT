bed555b7e34fc94d8228baf32f019c7427f8989a
satyajit9178 <167549811+guddu-debasis@users.noreply.github.com>
2025-08-18 16:38:05 +0530

    model fix

--- model_setup.py
+++ model_setup.py
@@ -1,13 +1,13 @@
 # model_setup.py
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-# from langchain_groq import ChatGroq
+from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_model():
-    model_name = "gemini-2.5-flash"
+    model_name = "llama-3.1-8b-instant"
     
-    return ChatGoogleGenerativeAI(model=model_name)
+    return ChatGroq(model=model_name)
 
